661d22f56b9b41bb9d084e404ebfce70dd63f6ef
andygqw <andygqwang@gmail.com>
2024-03-17 09:00:28 -0700

    added edit memo and more bug fixes

--- BlazorApp2Test/Data/MemoData.cs
+++ BlazorApp2Test/Data/MemoData.cs
@@ -21,29 +21,6 @@ namespace BlazorApp2Test.Data
 
         public async Task SaveMemo(MemoModel memo)
         {
-            //var memos = await LoadMemos();
-
-            //MemoModel lastElement;
-
-            //if (memos.Count > 0)
-            //{
-            //    lastElement = new MemoModel(memos[memos.Count - 1]);
-            //}
-            //else
-            //{
-            //    lastElement = new MemoModel();
-            //    lastElement.Id = 0;
-            //}
-
-            //memo.Id = lastElement.Id + 1;
-
-            //// Add the new memo to the list
-            //memos.Add(memo);
-
-            //// Serialize and save the updated list
-            //var serializedData = JsonSerializer.Serialize(memos);
-            //await File.WriteAllTextAsync(Helper.MemoJSONset, serializedData);
-
             Memo newMemo = new Memo();
 
             if (_userService.GetAuth())
@@ -67,21 +44,6 @@ namespace BlazorApp2Test.Data
 
         public async Task<List<MemoModel>> LoadMemos()
         {
-            //if (File.Exists(Helper.MemoJSONset))
-            //{
-            //    var jsonString = await File.ReadAllTextAsync(Helper.MemoJSONset);
-            //    if (string.IsNullOrWhiteSpace(jsonString))
-            //    {
-            //        return new List<MemoModel>();
-            //    }
-            //    var results = JsonSerializer.Deserialize<List<MemoModel>>(jsonString);
-            //    if(results != null)
-            //    {
-            //        return results;
-            //    }
-            //}
-            //return new List<MemoModel>();
-
             if (_userService.GetAuth())
             {
                 List<MemoModel> list = new List<MemoModel>();
@@ -114,50 +76,34 @@ namespace BlazorApp2Test.Data
             }
         }
 
+        public async Task EditMemo(MemoModel memo)
+        {
+            var m = await _context.Memos.FindAsync(memo.Id);
+
+            if(m != null)
+            {
+                if(m.image != null)
+                {
+                    DeleteImg(m.image);
+                }
+
+                m.name = memo.Name;
+                m.description = memo.Description;
+                m.image = memo.Image;
+
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new Exception("Can't find memo to be edited");
+            }
+        }
+
         public async Task DeleteMemo(MemoModel memo)
         {
-            //var MemoList = await LoadMemos();
-
-            //// Dunno why List.Contains() and .Remove() doesnt work
-            //if (MemoList.Any(m => m.Id == memo.Id))
-            //{
-            //    int index = MemoList.FindIndex(m => m.Id == memo.Id);
-
-            //    MemoList.RemoveAt(index);
-
-            //    if(MemoList.Any(m => m.Id == memo.Id))
-            //    {
-            //        throw new Exception("Remove failed");
-            //    }
-
-            //    var serializedData = JsonSerializer.Serialize(MemoList);
-            //    await File.WriteAllTextAsync(Helper.MemoJSONset, serializedData);
-
-            //    if (memo.Image != null)
-            //    {
-            //        char separator = '\\';
-            //        string[] parts = memo.Image.Split(separator);
-            //        var name = parts[parts.Length - 1];
-
-            //        var filePath = Helper.MemoImgset + '/' + name;
-
-            //        File.Delete(filePath);
-            //    }
-            //}
-            //else
-            //{
-            //    throw new Exception("Delete Memo which doesn't exist");
-            //}
-
-            if (memo.Image != null)
+            if(memo.Image != null)
             {
-                char separator = '\\';
-                string[] parts = memo.Image.Split(separator);
-                var name = parts[parts.Length - 1];
-
-                var filePath = Helper.MemoImgset + '/' + name;
-                
-                File.Delete(filePath);
+                DeleteImg(memo.Image);
             }
 
             var m = await _context.Memos.FindAsync(memo.Id);
@@ -188,6 +134,20 @@ namespace BlazorApp2Test.Data
             await using FileStream fs = new(path, FileMode.Create);
             await selectedFile.OpenReadStream(selectedFile.Size).CopyToAsync(fs);
             fs.Close();
+        }
+
+        private void DeleteImg(string path)
+        {
+            if (!string.IsNullOrEmpty(path))
+            {
+                char separator = '\\';
+                string[] parts = path.Split(separator);
+                var name = parts[parts.Length - 1];
+
+                var filePath = Helper.MemoImgset + '/' + name;
+
+                File.Delete(filePath);
+            }
         }
     }
 }
