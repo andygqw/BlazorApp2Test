773480b2bfb13d5c797f361c1471f08233022291
andygqw <andygqwang@gmail.com>
2024-03-17 21:38:37 -0700

    added objects

---
+++ BlazorApp2Test/Models/Memo.cs
@@ -0,0 +1,13 @@
+﻿
+namespace BlazorApp2Test.Models
+{
+    public class Memo
+    {
+        public int id { get; set; }
+        public int userId { get; set; }
+        public string? name { get; set; }
+        public string? description { get; set; }
+        public string? image { get; set; }
+        public DateTime? createTime { get; set; }
+    }
+}
---
+++ BlazorApp2Test/Models/Reply.cs
@@ -0,0 +1,19 @@
+﻿
+namespace BlazorApp2Test.Models
+{
+	public class Reply
+	{
+		public int id { get; set; }
+		public int userId { get; set; }
+		public int memoId { get; set; }
+		public int? replyId { get; set; }
+		public string? content { get; set; }
+		public DateTime? create_time { get; set; }
+
+		public Reply()
+		{
+			replyId = null;
+			content = null;
+		}
+	}
+}
