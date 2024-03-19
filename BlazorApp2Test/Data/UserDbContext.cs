303a668c4a86403586438abcd2e48f7208c39de8
andygqw <andygqwang@gmail.com>
2024-03-18 20:01:21 -0700

    version 2.2

--- BlazorApp2Test/Data/UserDbContext.cs
+++ BlazorApp2Test/Data/UserDbContext.cs
@@ -6,9 +6,9 @@ namespace BlazorApp2Test.Data
     public class UserDbContext : DbContext
     {
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<Memo> Memos { get; set; }
-        public DbSet<Reply> Replies { get; set; }
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Memo>? Memos { get; set; }
+        public DbSet<Reply>? Replies { get; set; }
 
         public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
         {
