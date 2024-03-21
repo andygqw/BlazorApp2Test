3de754fbdea1f0a0fe27a5ed80dc2d9f4851fb57
andygqw <andygqwang@gmail.com>
2024-03-20 21:11:04 -0700

    added Userfile type

---
+++ BlazorApp2Test/Models/UserFile.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BlazorApp2Test.Models
+{
+	public class UserFile
+	{
+		public int id { get; set; }
+		public int userId { get; set; }
+		public string? fileName { get; set; }
+		public string? filePath { get; set; }
+		public DateTime? createTime { get; set; }
+	}
+}
